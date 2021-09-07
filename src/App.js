84918dcd829d44c7710968caccb55fd7ea4fc4fe
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-07 23:18:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import SearchBar from './components/SearchBar/SearchBar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import ImageGalleryItem from './components/ImageGalleryItem/ImageGalleryItem';
-import Button from './components/Button/Button';
-import Modal from './components/Modal/Modal';
-import './App.css';
-import './components/image-finder/styles.css'
+import logo from "./logo.svg";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageGalleryItem from "./components/ImageGalleryItem/ImageGalleryItem";
+import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
+import "./App.css";
+import "./components/image-finder/styles.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <SearchBar />
-      <ImageGallery />
-      <ImageGalleryItem />
-      {/* <Button>
-        <button type="button"></button>
-        </Button> */}
-      {/* <Modal/> */}
-   </div>
-  );
+export default class App extends Component {
+  state = {
+  picture:''
 }
 
-export default App;
+  handleFormSubmit = picture => {
+    
+   this.setState({picture})
+ }
+//  componentDidMount() {
+//     const myKey = "22597300-51a9bfff07e627635843c3062";
+   
+//   }
+  render() {
+    return (
+      <div>
+        <SearchBar submit={ this.handleFormSubmit}/>
+       <ImageGallery />
+        <ImageGalleryItem />
+        {/* <Button>
+        <button type="button"></button>
+        </Button> */}
+        {/* <Modal/> */}
+      </div>
+    );
+  }
+}
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,21 +1,46 @@
- function SearchBar() {
+import { Component } from "react";
+
+
+export default class SearchBar extends Component{
+  state = {
+    picture:''
+  }
+
+  handleSearcheChange = e => {
+  this.setState({picture: e.currentTarget.value.toLowerCase()})
+  }
+  
+  
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.picture.trim() === '') {
+      return
+    }
+    this.props.submit(this.state.picture)
+    this.setState({ picture:''})
+    }
+
+  render() {
     return (
         <header className="Searchbar">
-  <form className="SearchForm">
+  <form className="SearchForm" onSubmit={this.handleSubmit}>
     <button type="submit" className="SearchForm-button">
       <span className="SearchForm-button-label">Search</span>
     </button>
 
     <input
       className="SearchForm-input"
-      type="text"
-      autocomplete="off"
-      autofocus
+            type="text"
+           
+            value={this.state.picture}
+            onChange={this.handleSearcheChange}
+      autoComplete="off"
+      autoFocus
       placeholder="Search images and photos"
     />
   </form>
 </header>
     )
-}
+  }
 
-export default SearchBar
+}

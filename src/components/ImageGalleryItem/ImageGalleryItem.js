adb2c927de016a01712d9fd13cea652841a4b2f1
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-08 13:31:41 +0100

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,10 +1,35 @@
+import { Component } from "react";
 
-function ImageGalleryItem(pictureUrl, name) {
+export default class ImageGalleryItem extends Component {
+  state = {
+    pictures: null,
+    loading: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevProps.pictures;
+    const nextSearch = this.props.pictures;
+    const keyApi = "22597300-51a9bfff07e627635843c3062";
+    if (prevSearch !== nextSearch) {
+      this.setState({ loading: true });
+      fetch(
+        `https://pixabay.com/api/?q=${nextSearch}&key=${keyApi}&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then((res) => res.json())
+        .then((pictures) => this.setState({ pictures }))
+        .finally(() => this.setState({ loading: false }));
+    }
+  }
+  render() {
+
     return (
-       <li className="ImageGalleryItem">
-  <img src={pictureUrl} alt={name} className="ImageGalleryItem-image" />
-</li> 
-  )
+      <li className="ImageGalleryItem">
+        <img
+          src={this.state.pictures}
+          alt="i"
+          className="ImageGalleryItem-image"
+        />
+      </li>
+    );
+  }
 }
-
-export default ImageGalleryItem

6fe81188ef18e9e3b93e4595a2eeffcf1fb61abf
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-08 13:44:37 +0100

    update

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -4,6 +4,7 @@ export default class ImageGalleryItem extends Component {
   state = {
     pictures: null,
     loading: false,
+    error:''
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -12,24 +13,40 @@ export default class ImageGalleryItem extends Component {
     const keyApi = "22597300-51a9bfff07e627635843c3062";
     if (prevSearch !== nextSearch) {
       this.setState({ loading: true });
-      fetch(
-        `https://pixabay.com/api/?q=${nextSearch}&key=${keyApi}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then((res) => res.json())
-        .then((pictures) => this.setState({ pictures }))
-        .finally(() => this.setState({ loading: false }));
+      setTimeout(() => {
+        fetch(
+          `https://pixabay.com/api/?q=${nextSearch}&key=${keyApi}&image_type=photo&orientation=horizontal&per_page=12`
+        )
+          .then((res) => {
+            if (res.ok) {
+              res.json();
+            }
+            return Promise.reject(
+              new Error(`no pictures with this name ${nextSearch}`)
+            );
+          })
+          .then((pictures) => this.setState({ pictures }))
+          .catch(error=>this.state({error}))
+          .finally(() => this.setState({ loading: false }));
+      }, 1000);
     }
   }
   render() {
-
+    const { pictures, loading } = this.state;
+    const{error}=this.props
     return (
       <li className="ImageGalleryItem">
-        <img
-          src={this.state.pictures}
+        {error && <h1>{error.message}</h1>}
+        {loading && <div>Loading...</div>}
+        {pictures && (
+           <img
+          src={pictures}
           alt="i"
           className="ImageGalleryItem-image"
         />
-      </li>
+     
+       )}
+       </li> 
     );
   }
 }

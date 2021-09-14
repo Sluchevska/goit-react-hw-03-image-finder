84b01f0111f841d95a076d8da8d1550a0dcdc818
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-14 15:46:38 +0100

    update

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com'
 
 export const fetchPics = async (pictureName, page) => {
-    const keyApi = '22597300-51a9bfff07e627635843c3062';
-    const response = await axios.get(
-      `https://pixabay.com/api/?q=${pictureName}&page=${page}&key=${keyApi}&image_type=photo&orientation=horizontal&per_page=12`,
-    );
-    return response.data.hits;
-  };
+  const keyApi = '22597300-51a9bfff07e627635843c3062';
+  const response = await axios.get(
+    `/api/?q=${pictureName}&page=${page}&key=${keyApi}&image_type=photo&orientation=horizontal&per_page=12`,
+  );
+  return response.data.hits;
+};

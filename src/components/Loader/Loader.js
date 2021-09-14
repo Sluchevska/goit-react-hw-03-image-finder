843a55e033f7c043c0f4073dcca816cf27332566
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-14 16:15:23 +0100

    update

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,8 @@
-import { Component } from "react";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-export default class App extends Component {
-  //other logic
-  render() {
-    return (
-<Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
-    );
+export default function App()  {
+
+    return <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+  
   }
-}
